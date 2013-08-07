9db63f9a1d78588b66768bc9986fdf5f1dd745c2
Rodel Hidalgo <hidalgo.rodel@gmail.com>
2013-08-07 11:36:15 +0800

    implement services on PurchaseOrderController

--- VG SCFC-financing-cis-ui/src/com/vg/scfc/financing/cis/ui/controller/PurchaseOrderController.java
+++ VG SCFC-financing-cis-ui/src/com/vg/scfc/financing/cis/ui/controller/PurchaseOrderController.java
@@ -5,8 +5,10 @@
  */
 package com.vg.scfc.financing.cis.ui.controller;
 
+import com.vg.commons.util.DateUtil;
 import com.vg.scfc.financing.cis.ent.PurchaseOrder;
-import com.vg.scfc.financing.cis.service.PurchaseOrderService;
+import com.vg.scfc.financing.cis.ui.settings.UISetting;
+import com.vg.scfc.financing.cis.ui.validator.UIValidator;
 import java.math.BigDecimal;
 
 /**
@@ -24,8 +26,11 @@ public class PurchaseOrderController {
         return instance;
     }
     
-    public Object createNew(String unitApplied, BigDecimal downpayment, BigDecimal insurance, int term, BigDecimal amortization, boolean isBrandNew, String purpose, String remarks, String status) {
+    public Object createNew(String unitApplied, BigDecimal downpayment, BigDecimal insurance, int term, 
+            BigDecimal amortization, boolean isBrandNew, String purpose, String remarks, String status) {
+        Object result = null;
         PurchaseOrder po = new PurchaseOrder();
+        po.setEncodeDate(DateUtil.now());
         po.setUnitApplied(unitApplied);
         po.setDownPayment(downpayment.doubleValue());
         po.setInsurance(insurance.doubleValue());
@@ -35,12 +40,32 @@ public class PurchaseOrderController {
         po.setPurpose(purpose);
         po.setRemarks(remarks);
         po.setStatus(status);
+        /* For trail & Liveupdate */
+        po.setLocation(UISetting.getStoreLocation());
+        po.setStation(UISetting.getComputerName());
+        po.setUser(UISetting.getSystemUser());
         
-        return new Object();
+        try {
+            result = UISetting.getPurchaseOrderService().insert(po);
+        } catch (Exception ex) {
+            UIValidator.log(ex, PurchaseOrderController.class);
+        }
+        
+        return result;
     }
     
     public Object update() {
         return new Object();
+    }
+    
+    public Object findByFormNo(String formNo) {
+        Object result = null;
+        try {
+            result = UISetting.getPurchaseOrderService().findByTransNo(formNo);
+        } catch (Exception ex) {
+            UIValidator.log(ex, PurchaseOrderController.class);
+        }
+        return result;
     }
 
 }
